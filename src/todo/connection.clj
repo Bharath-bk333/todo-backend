1c4f7e55710dca049f47e08ea4434f63599c91f7
Sumit <kumar1993sumit15@gmail.com>
2017-12-19 23:37:03 +0530

    mongo database queries & connection file added

--- src/todo/connection.clj
+++ src/todo/connection.clj
@@ -1,1 +1,16 @@
-(ns todo.connection)
+(ns todo.connection
+  (:require [monger.core :as mg]))
+
+(defn connect
+  []
+  (mg/connect))
+
+(defn get-coll
+  "Connect to the db collection"
+  [conn coll-name]
+  (mg/get-db conn coll-name))
+
+(defn disconnect
+  "Disconnect & close connection"
+  [conn]
+  (mg/disconnect conn))
--- src/todo/query.clj
+++ src/todo/query.clj
@@ -1,1 +1,96 @@
-(ns todo.query)
+(ns todo.query
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [monger.operators :refer :all]
+            [todo.connection :as tcn])
+  (:import org.bson.types.ObjectId))
+
+(def bucket {:tasks "taskscoll"}) ;collection names in db -do not change
+
+(def coll-task "taskdata")
+
+(defn create-user
+  [user]
+  (let [conn (tcn/connect)
+        db (tcn/get-coll conn coll-task)
+        ]
+    (let [res (mc/insert-and-return db (:tasks bucket)
+                                    {:user user
+                                     :tasks []})
+          ]
+      (mg/disconnect conn)
+      (not (nil? res)))))
+
+(defn insert-task
+  [user task]
+  (let [conn (tcn/connect)
+        db (tcn/get-coll conn coll-task)
+        ]
+    (let [res (mc/update db (:tasks bucket)
+                         {:user user}
+                         {$push {:tasks task}}
+                         {:upsert true})
+          ]
+      (mg/disconnect conn)
+      (not (nil? res)))))
+
+(defn delete-task
+  [user taskid]
+  (let [conn (tcn/connect)
+        db (tcn/get-coll conn coll-task)
+        ]
+    (let [res (mc/update
+                db
+                (:tasks bucket)
+                {:user user}
+                {$pull {:tasks {:tid taskid}}})
+          ]
+      (mg/disconnect conn)
+      (not (nil? res)))))
+
+(defn update-task
+  "updates a certain field of task"
+  [user taskid updatekeys newvalues]
+  (let [conn (tcn/connect)
+        db (tcn/get-coll conn coll-task)
+        nmap (zipmap (mapv #(str "tasks.$." %) updatekeys) newvalues)
+        ]
+    (println "nmappp" nmap)
+    (let [res (mc/update
+                db
+                (:tasks bucket)
+                {:user user
+                 :tasks.tid taskid}
+                {$set nmap})
+          ]
+      (println "the result" res)
+      (mg/disconnect conn)
+      (not (nil? res)))))
+
+(defn- fetch-db-data
+  [condition-map]
+  (let [conn (tcn/connect)
+        db (tcn/get-coll conn coll-task)
+        ]
+    (let [res (mc/find-one-as-map
+                db
+                (:tasks bucket)
+                condition-map)
+          ]
+      (mg/disconnect conn)
+      res)))
+
+(defn test
+  []
+  (let [conn (tcn/connect)
+        db (tcn/get-coll conn coll-task)
+        ]
+    (let [res (mc/update
+                db
+                (:tasks bucket)
+                {:user "sumit"
+                 :tasks.tid "RPKVVKTSD"}
+                {$set {:tasks.title "taskman"}})
+          ]
+      (mg/disconnect conn)
+      res)))
